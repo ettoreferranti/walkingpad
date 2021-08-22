eeaa1d7f00054e7f8429caaf32d38756d46e5614
Ettore Ferranti <ettore.ferranti@gmail.com>
2021-08-22 21:17:28 +0200

    removed R1 from address

--- scanner.py
+++ scanner.py
@@ -33,7 +33,7 @@ def main():
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
-    loop.run_until_complete(print_services("57:4C:4D:1E:21:AD: R1 Pro"))
+    loop.run_until_complete(print_services("57:4C:4D:1E:21:AD"))
 
     
 
