8187bb6c76522069135906f75c2d9b340d5b2731
Ettore Ferranti <ettore.ferranti@gmail.com>
2021-08-24 17:01:17 +0100

    added bt config files (for record)

--- api.py
+++ api.py
@@ -17,6 +17,8 @@ name = "r1 pro"
 
 address = None
 
+walkingPad = None
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Home Office Manager</h1><p>This site offers an API to manage your home office remotely.</p>"
@@ -24,23 +26,33 @@ def home():
 @app.route('/api/v1/resources/walkingpad/status')
 async def status():
     global address
+    global walkingPad
+
+    await get_walking_pad()
+
+    #await walkingPad.connect()
+    await walkingPad.read_stats()
+    #await walkingPad.disconnect()
+
+    return jsonify(walkingPad.get_status())
+
+async def get_walking_pad():
+    global address
+    global walkingPad
 
     if address is None:
+        logger.info(f"Getting the walking pad address ({name})")
         address = await WalkingPad.get_address_by_name(name)
+        walkingPad = WalkingPad(address)
+        await walkingPad.connect()
 
-    walkingPad = WalkingPad(address)
-
-    await walkingPad.connect()
-    await walkingPad.read_stats()
-    await walkingPad.disconnect()
-    return jsonify(walkingPad.get_status())
 
 def main():
     #loop = asyncio.get_event_loop()
-    #loop.run_until_complete(initialise_pad())
+    #loop.run_until_complete(init_pad())
 
     app.run(host='0.0.0.0', port=3000)
-
+    
    
 if __name__ == "__main__":
     # execute only if run as a script
